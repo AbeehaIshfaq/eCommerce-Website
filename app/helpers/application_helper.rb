f471d10fdafa928b009409eac4cf6f5a1f778ff9
AbeehaIshfaq <92564822+AbeehaIshfaq@users.noreply.github.com>
2022-06-24 08:17:27 +0500

    Seller UI completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,9 @@ module ApplicationHelper
     def active_class(path)
         "active" if current_page?(path)
     end
-    
+    def is_seller?
+        # puts (@users.find(params[:role]))*100
+        # puts (users.find(params[:role]))*100
+        current_user.role == 'Seller'
+    end
 end
